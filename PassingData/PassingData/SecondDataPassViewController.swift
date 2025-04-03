0dc4024de87625ec44730061ed5dca83d78b168d
NagarjunaNaidu141 <nagarjunanaidu141@gmail.com>
2025-04-03 19:26:37 +0530

    "my head"

--- PassingData/PassingData/SecondDataPassViewController.swift
+++ PassingData/PassingData/SecondDataPassViewController.swift
@@ -9,21 +9,17 @@ import UIKit
 
 class SecondDataPassViewController: UIViewController {
 
+    @IBOutlet weak var textFielddata: UITextField!
+    var data : String?
     override func viewDidLoad() {
         super.viewDidLoad()
+        if  data != nil {
+            textFielddata.text = data
+            
+        }
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- PassingData/PassingData/ViewController.swift
+++ PassingData/PassingData/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dataLabel: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let controller = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SecondVC") as! SecondDataPassViewController
+        controller.data = dataLabel.text
+        present(controller, animated: true)
+    }
 }
 
