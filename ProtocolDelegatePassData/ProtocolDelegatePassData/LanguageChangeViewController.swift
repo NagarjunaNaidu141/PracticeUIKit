a1c998d8634e7c8ea4cd85b2c291220733a90fc8
NagarjunaNaidu141 <nagarjunanaidu141@gmail.com>
2025-04-03 20:14:19 +0530

    "protocol -delegate example data pasing"

---
+++ ProtocolDelegatePassData/ProtocolDelegatePassData/LanguageChangeViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LanguageChangeViewController.swift
+//  ProtocolDelegatePassData
+//
+//  Created by Nagarjuna Naidu on 03/04/25.
+//
+
+import UIKit
+
+class LanguageChangeVCDelegate: UIViewController {
+    var delegate: LanguageChangeDelegate?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func frenchLanguage(_ sender: UIButton) {
+        delegate.languageChanged(index: 0)
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func englishLanguage(_ sender: UIButton) {
+        delegate.languageChanged(index: 1)
+        
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func teluguLanguague(_ sender: UIButton) {
+        delegate.languageChanged(index: 0)
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+protocol LanguageChangeDelegate{
+    func languageChanged(index: Int)
+    
+}
---
+++ ProtocolDelegatePassData/ProtocolDelegatePassData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProtocolDelegatePassData
+//
+//  Created by Nagarjuna Naidu on 03/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
