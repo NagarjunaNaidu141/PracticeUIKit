cbde7f50447a7eff547933156389b5d7f315ec31
NagarjunaNaidu141 <nagarjunanaidu141@gmail.com>
2025-04-03 17:15:19 +0530

    "nn"

---
+++ PassData1/PassData1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassData1
+//
+//  Created by Nagarjuna Naidu on 03/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
